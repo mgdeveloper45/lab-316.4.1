517a19559391a01717011171af7f80ff67f6f0e9
MarkGee45 <moel55.mg@gmail.com>
2024-11-14 07:18:59 -0800

    changed querySelector to get element by id and created variables for form elements

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-const registerForm = document.querySelector("registration");
-const loginForm = document.querySelector("login");
-const errorDisplay = document.querySelector("errorDisplay");
+const registerForm = document.getElementById("registration");
+console.log(registerForm)
+const loginForm = document.getElementById("login");
+const errorDisplay = document.getElementById("errorDisplay");
 
 function showError(message, inputElement) {
     errorDisplay.innerText = message;
@@ -14,4 +15,11 @@ function clearError() {
 
 registerForm.addEventListener("submit", evt => {
     evt.preventDefault();
+    clearError();
+
+    const username = document.getElementById("register-username").value.trim();
+    const email = document.getElementById("register-email").value.trim();
+    const password = document.getElementById("register-password").value;
+    const passwordCheck = document.getElementById("register-password-check").value;
+    const termsAccepted = document.getElementById("terms").checked;
 }) 
